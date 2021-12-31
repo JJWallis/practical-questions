6aa4022b73b9737250c9c758fef8cbc8c1e6f06e
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-31 13:17:01 +0000

    Router setup ip

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React, { FC } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Home'
 import About from './About'
 
 const App: FC = () => {
    return (
       <BrowserRouter>
-         <Route path="./About.tsx" element={About} />
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+         </Routes>
       </BrowserRouter>
    )
 }
