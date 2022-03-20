e7f50857baadf827953e5ffba62004042939b654
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-20 17:16:25 +0000

    search input setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import './index.css'
 
 const App: FC = () => {
+   const [search, setSearch] = useState('')
+
    return (
       <main>
          <h1>My Shopping List</h1>
          <label htmlFor="search-input" className="label">
             Food search
          </label>
-         <input id="search-input" className="input" />
+         <input
+            id="search-input"
+            className="input"
+            value={search}
+            onChange={({ target }) => setSearch(target.value)}
+         />
       </main>
    )
 }
