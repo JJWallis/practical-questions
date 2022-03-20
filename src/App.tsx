6faa6ea251c4ed28a36d10fcdc1f5e3e132e4e2c
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-20 17:27:11 +0000

    ordered list results working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React, { FC, useState } from 'react'
 import './index.css'
 
+// link same idx as item viewing in results list with its btn's tab index
+
 const App: FC = () => {
    const [search, setSearch] = useState('')
 
    return (
       <main>
          <h1>My Shopping List</h1>
-         <fieldset>
-            <legend>Shopping list form</legend>
-            <label htmlFor="search-input" className="label">
-               Food search
-            </label>
-            <input
-               id="search-input"
-               className="input"
-               value={search}
-               onChange={({ target }) => setSearch(target.value)}
-            />
-         </fieldset>
+         <label htmlFor="search-input" className="label">
+            Food search
+         </label>
+         <input
+            id="search-input"
+            className="input"
+            value={search}
+            onChange={({ target }) => setSearch(target.value)}
+         />
+         <ol>
+            <li>
+               <button>bread</button>
+            </li>
+            <li>
+               <button>milk</button>
+            </li>
+         </ol>
       </main>
    )
 }
