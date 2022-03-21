96aa2639b69dac4ff5e09427a2e27065cc7086f7
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-21 19:49:33 +0000

    shoppingListActions type declaration in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,42 @@
 import React, { FC, useEffect, useReducer, useRef, useState } from 'react'
 import './index.css'
 
-type ShoppingList = null
-
+type ShoppingList = ShoppingListItem[] | []
 interface ShoppingListItem {
    id: number
    checked: boolean
+   value: string
 }
+type ShoppingListActions =
+   | {
+        type: 'ADD_ITEM'
+        payload: ShoppingListItem
+     }
+   | {}
+   | {}
 
 // link same idx as item viewing in results list with its btn's (add accessibility) tab index
 // reset input to empty str on result btn click + re-focus
 
-function retrieveShoppingList(key: string) {
+function retrieveShoppingList(key: string): ShoppingList {
    const prevList = localStorage.getItem(key)
    return prevList ? JSON.parse(prevList) : []
 }
 
-function reducer(action, state) {
+function reducer(action, state: ShoppingList) {
    const { type } = action
    switch (state) {
       case type: {
          return {}
       }
-   }
-
-   switch (key) {
-      case value:
-         break
-
-      default:
-         break
+      default: {
+         return state
+      }
    }
 }
 
 const App: FC = () => {
-   const [shoppingList, dispatch] = useReducer(
+   const [shoppingList, dispatch] = useReducer<ShoppingList>(
       reducer,
       [],
       retrieveShoppingList
