ffdc3f9d97c3f1371abf7655a193e7cdc53bb51e
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-28 12:19:07 +0100

    leetcode - flipAndConvertImg setuo

--- src/About.tsx
+++ src/About.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
 
+function flipAndInvertImage(image: number[][]): number[][] {
+   return image.reverse()
+}
+
+console.log(
+   flipAndInvertImage([
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+   ])
+)
+
 const About: React.FC = () => {
    return <></>
 }
