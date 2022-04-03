24fc25210dc7e89bd6ee9409d3688d4512884d2a
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-03 16:57:59 +0100

    frontendeval word guessing game comments setup

--- src/About.tsx
+++ src/About.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+// user has 6 attempts (wins if guesses correct < 6)
+// highlight each try => yellow if letter present + wrong position
+// green => letter present + correct position | greyed out => letter not present
+
 const About = () => {
    return <></>
 }
