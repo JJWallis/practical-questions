97e34e6dd18e07abcd707bb555aea7a43b322cb8
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-10 13:03:56 +0100

    carousel - button comp created

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
+const Button: React.FC = () => {
+   return <button>ss</button>
+}
+
 const Carousel: React.FC = () => {
-   return <main></main>
+   return (
+      <main>
+         <div>{/* <img src="" alt="" /> */}</div>
+      </main>
+   )
 }
 
 export default Carousel
