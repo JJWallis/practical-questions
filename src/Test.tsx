a7d0fcd457748bfb29e48b81bd3f39167879acca
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-10 16:09:57 +0100

    comments added

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Test = () => {
+   const [comments, setComments] = React.useState<string[]>([])
    const [comment, setComment] = React.useState('')
 
    return (
@@ -11,9 +12,19 @@ const Test = () => {
                value={comment}
                onChange={(e) => setComment(e.target.value)}
             />
-            <input type="button" value="Post" />
+            <input
+               type="button"
+               value="Post"
+               onClick={() =>
+                  comment.length && setComments([...comments, comment])
+               }
+            />
          </form>
-         <ul>{/* comments */}</ul>
+         <ul>
+            {comments.map((comment, idx) => (
+               <li key={idx}>{comment}</li>
+            ))}
+         </ul>
       </div>
    )
 }
