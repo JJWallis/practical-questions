2e0da39f97a965b8b4e5bfb4440848551272754e
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-10 16:06:54 +0100

    state setuo

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
 const Test = () => {
-   const [comment, setComment] = React.useState()
+   const [comment, setComment] = React.useState('')
 
    return (
       <div>
          <form>
-            <input type="text" />
+            <input
+               type="text"
+               value={comment}
+               onChange={(e) => setComment(e.target.value)}
+            />
             <input type="button" value="Post" />
          </form>
          <ul>{/* comments */}</ul>
