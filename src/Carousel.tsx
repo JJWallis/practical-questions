b77e44dc729868163584cb7dad990e8b110032e6
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-10 13:16:31 +0100

    carousel - data fetched correctly

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,14 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Carousel: React.FC = () => {
    const [images, setImages] = useState([])
 
    const fetchImgs = async () => {
       try {
+         const res = await fetch('https://www.reddit.com/r/aww/top/.json?t=all')
+         const { data } = await res.json()
+         setImages(data.children)
       } catch (error) {
          console.error(error)
       }
    }
+
+   useEffect(() => {
+      fetchImgs()
+   }, [])
+
+   useEffect(() => console.log(images))
 
    return (
       <main>
