83ce7178539a1749868b65aedfc0b598beffc62f
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-10 13:24:22 +0100

    carousel - btns correctly updating active img state

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -5,9 +5,13 @@ const Carousel: React.FC = () => {
    const [activeImg, setActiveImg] = useState(0)
 
    const updateImg = (index: number) => {
-      const res = images[index].data
-      //   console.log(data)
+      const data = images[index].data
+      console.log(data)
    }
+
+   const increment = () => setActiveImg((prev) => prev + 1)
+
+   const decrement = () => setActiveImg((prev) => prev - 1)
 
    useEffect(() => {
       const fetchImgs = async () => {
@@ -31,8 +35,18 @@ const Carousel: React.FC = () => {
       <main>
          <div className="image-ct">
             {/* <img src="" alt="" /> */}
-            <button className="btn btn-right">&rarr;</button>
-            <button className="btn btn-left">&larr;</button>
+            <button
+               className="btn btn-right"
+               onClick={() => activeImg < images.length && increment()}
+            >
+               &rarr;
+            </button>
+            <button
+               className="btn btn-left"
+               onClick={() => activeImg > 0 && decrement()}
+            >
+               &larr;
+            </button>
          </div>
       </main>
    )
