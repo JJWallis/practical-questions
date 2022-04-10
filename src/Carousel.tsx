ae8dece4315ab94b82d45e71eefadb0b35aeabd8
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-10 13:08:26 +0100

    carousel - right arrow button created

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 
-const Button: React.FC = () => {
-   return <button>ss</button>
-}
-
 const Carousel: React.FC = () => {
    return (
       <main>
          <div className="image-ct">{/* <img src="" alt="" /> */}</div>
+         <button>&rarr;</button>
       </main>
    )
 }
