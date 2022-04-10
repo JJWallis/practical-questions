28471bbf45392f0d63c3b9fcd15cdacc91d09a7e
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-10 15:03:48 +0100

    test comp setup

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import About from './About'
 import Carousel from './Carousel'
+import Test from './Test'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 ReactDOM.render(
@@ -12,6 +13,7 @@ ReactDOM.render(
             <Route path="/" element={<App />} />
             <Route path="/about" element={<About />} />
             <Route path="/carousel" element={<Carousel />} />
+            <Route path="/test" element={<Test />} />
          </Routes>
       </BrowserRouter>
    </React.StrictMode>,
