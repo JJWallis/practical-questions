9e903f7ea157eed3fc31f2387b368f3a0ab66e89
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-21 09:29:20 +0100

    dotCoordinates converted to arr

--- src/Dice.tsx
+++ src/Dice.tsx
@@ -3,14 +3,12 @@ import React, { useState } from 'react'
 const MAX_DICE = 6
 const TOTAL_NUM = 99
 
-const dotCoordinates = {
-   1: '20:20 30:30',
-   // 2: '',
-   // 3: '',
-   // 4: '',
-   // 5: '',
-   // 6: '',
-}
+const dotCoordinates = [
+   '20:20',
+   '30:30 40:40',
+   '50:50 60:60 70:70',
+   '80:80 90:90 100:100 120:120',
+]
 
 const Dice: React.FC = () => {
    const [dice, setDice] = useState([])
@@ -22,9 +20,9 @@ const Dice: React.FC = () => {
    }
 
    const produceDice = () => {
-      const random = Math.floor(Math.random() * TOTAL_NUM) + 1
+      const randomNum = Math.floor(Math.random() * TOTAL_NUM) + 1
       //   dotCoordinates[String(randomNum) as keyof] => return dice +
-      //  split (' ') + map() over arr within to produce
+      //   split (' ') + map() over arr within to produce
       //   dots inside using each val (inc key)
    }
 
@@ -39,7 +37,6 @@ const Dice: React.FC = () => {
          />
          <button onClick={produceDice}>Role</button>
          <div role="grid" className="dice-ct">
-            {produceDice}
             <div role="gridcell" className="dice" aria-label={'2 sided dice'}>
                <div className="dice-dot"></div>
             </div>
